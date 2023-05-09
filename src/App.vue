78a10be681b737c37183f63d90a7fb78b9cf18dc
Francesca-23 <francescadidomenico4@gmail.com>
2023-05-09 22:42:40 +0200

    aggiunta serie tv e immagini di copertina

--- src/App.vue
+++ src/App.vue
@@ -19,7 +19,7 @@
     methods: {
 
       movieApi(){
-        axios.get(`https://api.themoviedb.org/3/search/movie?api_key=1d030cea23b1388bde4c590ce01bf4eb&language=IT&query=${store.film}`).then((res) => {
+        axios.get(`https://api.themoviedb.org/3/search/multi?api_key=1d030cea23b1388bde4c590ce01bf4eb&language=IT&query=${store.film}`).then((res) => {
           this.store.movieArray = res.data
         })
       }
--- src/store.js
+++ src/store.js
@@ -5,6 +5,5 @@ export const store = reactive(
         movieArray: [],
         film: '',
         api_key: '1d030cea23b1388bde4c590ce01bf4eb',
-        flagArray: '',
     }
 )
